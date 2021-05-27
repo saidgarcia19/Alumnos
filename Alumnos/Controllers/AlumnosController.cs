5f5943ebe33e9a8ef63d5ab16d39cecee67f760a
Said <garciam.said@gmail.com>
2021-05-26 21:42:41 -0500

    Agregar archivos de proyecto.

---
+++ Alumnos/Controllers/AlumnosController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Alumnos.Models;
+using System.IO;
+namespace Alumnos.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Registro(Alumno alumno)
+        {
+            try
+            {
+                TextWriter registro = new StreamWriter("C:\\Users\\said_\\source\\repos\\Alumnos\\Alumnos\\Archivos\\Alumnos\\" + alumno.RFC.ToString() + ".txt");
+                registro.WriteLine(alumno.Nombre.ToString());
+                registro.WriteLine(alumno.ApellidoPaterno.ToString());
+                registro.WriteLine(alumno.ApellidoMaterno.ToString());
+                registro.WriteLine(alumno.FechaNacimiento.ToString());
+                registro.WriteLine(alumno.Genero.ToString());
+                registro.WriteLine(alumno.FechaIngreso.ToString());
+                registro.WriteLine(alumno.Activo.ToString());
+                registro.WriteLine(alumno.RFC.ToString());
+
+                registro.Close();
+                return "1";
+            }
+            catch (Exception ex)
+            {
+
+                return "Ocurrio un error" + ex;
+            }
+            
+        }
+    }
+}
---
+++ Alumnos/Models/Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alumnos.Models
+{
+    public class Alumno
+    {
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Genero { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public bool Activo { get; set; }
+        public string RFC { get; set; }
+    }
+}
